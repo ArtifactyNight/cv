d6a1601a1057ed5ed4cc7cdd98d3095e05b91a48
ArtifactyNight <itsnight134679@gmail.com>
2024-12-18 00:05:13 +0700

    chore: Update website metadata descriptions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Kidsanaphon Kaeopha - cv",
-  description: "Student, Freelancer and Gamer",
+  description: "Student & Freelancer",
   keywords: [
     "NightKunGz",
     "Kidsanaphon Kaeopha",
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
   creator: "Kidsanaphon Kaeopha",
   openGraph: {
     title: "Kidsanaphon Kaeopha - cv",
-    description: "Student, Freelancer and Gamer",
+    description: "Student & Freelancer",
     type: "website",
     locale: "en_US",
     siteName: "Kidsanaphon Kaeopha Portfolio",
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Kidsanaphon Kaeopha - cv",
-    description: "Student, Freelancer and Gamer",
+    description: "Student & Freelancer",
   },
   robots: {
     index: true,
