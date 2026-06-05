11d4f26e58e89fcd50e500726a14208a6fe8b51c
ArtifactyNight <itsnight321789@gmail.com>
2026-06-06 00:33:01 +0700

    chore: Update site URL in Astro configuration
    - Changed the site URL from "https://www.example.com" to "https://nightz.me" in the Astro configuration file for accurate project representation.

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.example.com",
+  site: "https://nightz.me",
   integrations: [mdx(), sitemap(), icon(), react()],
   vite: {
     plugins: [tailwindcss()],
